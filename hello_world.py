b46882dfbdbc26fb4cd93f7d58356907919d61e5
STEAMAN1 <gohgoh0512@gmail.com>
2024-09-12 17:41:10 +0900

    test2
    test2

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,12 @@
-def print_hello():
-	print("Hello World!!")
+def add(x, y):
+	a = x + y
+	return a
 
-if __name__ == '__main__':
-	print_hello()
+
+def print_KIT():
+	print("KIT")
+
+
+if __name__ == "__main__":
+	print_KIT()
+	print( print_hello(1,2) )
